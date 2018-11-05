10d3b2d7ef50eb3c7428265ad7463c3f2a1db2e8
Aluno <karen.violim2011@gmail.com>
2018-11-05 21:35:36 -0200

    Criação do Site utilizando PHP

---
+++ 3_trimestre/aula32.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Aula 32: Criando sites com PHP</title>
+  </head>
+  <body>
+    <?php include("menu.php"); ?>
+
+    <h2>Exemplo de site no PHP</h2>
+    <p>Você pode colocar todo seu html dentro deste arquivo PHP que vai funcionar
+    da mesma forma.</p>
+    <p>Para acessar seu site, vá em
+    http://localhost/tecnologias_web/3_trimestre/aula32.php</p>
+
+  </body>
+</html>
